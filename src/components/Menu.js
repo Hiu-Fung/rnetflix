07650fc30bb6016c1dbd13789816932b98051c59
Hiu-Fung Hau <dafungz@htomail.com>
2018-06-23 14:21:25 +0200

    Add Menu component (WiP)

---
+++ src/components/Menu.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import {
+    Dimensions,
+    StyleSheet,
+    View,
+    Text,
+    Image,
+    ScrollView,
+} from 'react-native';
+
+import Icons from 'react-native-vector-icons/FontAwesome';
+
+const { width, height } = Dimensions.get('window');
+
+class Menu extends Component {
+    render() {
+        return (
+            <View />
+        );
+    }
+}
+
+
+export default Menu;
