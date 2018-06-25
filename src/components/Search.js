7c73124fc1a0d09720df8c2ab08de9a669997802
Hiu-Fung Hau <dafungz@htomail.com>
2018-06-25 14:46:57 +0200

    Add Search component (WiP)

---
+++ src/components/Search.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet
+} from 'react-native';
+
+class Search extends Component {
+    render(){
+        return (
+          <Text>Search</Text>
+        );
+    }
+}
+
+export default Search;
